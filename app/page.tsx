060e49ce3e531ef3346ce56a0b1cc6fdeb66e689
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-22 23:08:35 +0530

    Skill Component Added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import {Appbar} from "@/components/Appbar"
 import {Hero} from "@/components/Hero"
 import {About} from "@/components/About"
 import { Footer} from "@/components/Footer"
+import { Skills } from "@/components/Skills"
 export default function Home() {
   return (
    <div className="text-white min-h-screen bg-black">
@@ -10,6 +11,8 @@ export default function Home() {
       <main>
         <Hero/>
         <About/>
+
+        <Skills/>
       </main>
 
       <Footer/>
