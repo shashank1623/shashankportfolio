5ad4992ea93ece979c6256c32650165e221b8706
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-22 23:09:37 +0530

    Skills component added with Header

---
+++ components/Skills.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+export function Skills() {
+
+    return (
+        <section id="skills" className="py-20 bg-black">
+            <h2 className="text-4xl font-bold mb-12 text-center">
+                The <span className="text-purple-500">Tech</span> Behind My <span className="text-blue-500">Creations</span>
+            </h2>
+
+        </section>
+    )
+}
