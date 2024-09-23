e29ed2f373c5330086ffc0ec017c2f40bf799562
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-23 22:25:30 +0530

    Social Url added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,15 +7,15 @@ export function Footer () {
         <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
           <p>&copy; 2023 Code By The Ghost. All rights reserved.</p>
           <div className="flex space-x-4 mt-4 md:mt-0">
-            <a href="#" className="text-white hover:text-blue-300 transition-colors">
+            <a href="https://github.com/shashank1623" className="text-white hover:text-blue-300 transition-colors">
               <FaGithub size={24} />
               <span className="sr-only">GitHub</span>
             </a>
-            <a href="#" className="text-white hover:text-blue-300 transition-colors">
+            <a href="https://www.linkedin.com/in/shashank-bhardwaj-1a92b9213/" className="text-white hover:text-blue-300 transition-colors">
               <FaLinkedin size={24} />
               <span className="sr-only">LinkedIn</span>
             </a>
-            <a href="#" className="text-white hover:text-blue-300 transition-colors">
+            <a href="https://x.com/theghost1623" className="text-white hover:text-blue-300 transition-colors">
               <FaTwitter size={24} />
               <span className="sr-only">Twitter</span>
             </a>
