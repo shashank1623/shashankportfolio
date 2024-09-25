f494af59853a67388bb7d347b1b7fa9cb554c111
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-25 23:20:38 +0530

    Service component added to root layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import {Hero} from "@/components/Hero"
 import {About} from "@/components/About"
 import { Footer} from "@/components/Footer"
 import { Skills } from "@/components/Skills"
+import Service from "@/components/Services"
 export default function Home() {
   return (
    <div className="text-white min-h-screen bg-black">
@@ -13,6 +14,8 @@ export default function Home() {
         <About/>
 
         <Skills/>
+        <Service/>
+
       </main>
 
       <Footer/>
