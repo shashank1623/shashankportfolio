d923e19aa22e85cf91ffcd3d47ca510d2e5da6c4
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-26 09:43:24 +0530

    Extended Skill Section Added

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import {About} from "@/components/About"
 import { Footer} from "@/components/Footer"
 import { Skills } from "@/components/Skills"
 import Service from "@/components/Services"
+import SkillsInterface from "@/components/SkillsInterface"
 export default function Home() {
   return (
    <div className="text-white min-h-screen bg-black">
@@ -15,6 +16,8 @@ export default function Home() {
 
         <Skills/>
         <Service/>
+
+        <SkillsInterface/>
 
       </main>
 
