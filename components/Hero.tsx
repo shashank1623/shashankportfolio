38d1a647f1a569b5882a3bd184c8628775ed777b
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-26 22:29:54 +0530

    Button Content Update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -35,7 +35,7 @@ export function Hero() {
                 <Button borderRadius="1.8rem"
                     className='bg-blue-600 text-white px-8 py-3 rounded-full text-lg font-semibold hover:bg-blue-700 transition duration-300'
                 >
-                    Get in Touch
+                    Get Started
                 </Button>
 
                 <motion.div
