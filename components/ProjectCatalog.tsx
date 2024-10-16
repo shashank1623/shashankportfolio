c4f2034d1743d296f8b6d07972dc8f14adbe5b7d
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-10-16 21:15:25 +0530

    Updated Dialog Ui and content Strucuture

--- components/ProjectCatalog.tsx
+++ components/ProjectCatalog.tsx
@@ -45,7 +45,11 @@ function ProjectCard({ title, description, imageUrl, tags, detailedDescription, 
                 </DialogHeader>
                 <div className="space-y-4">
                     <h4 className="font-semibold text-lg mb-2">Description</h4>
-                    <p className="text-gray-300 mb-4 leading-relaxed">{detailedDescription}</p>
+                    <div className="text-gray-300 mb-4 leading-relaxed space-y-4">
+                        {detailedDescription.split('\n\n').map((paragraph, index) => (
+                            <p key={index}>{paragraph}</p>
+                        ))}
+                    </div>
 
                     <h4 className="font-semibold text-lg mb-2">Tech Stack:</h4>
                     <div className="flex flex-wrap gap-2">
@@ -71,6 +75,7 @@ function ProjectCard({ title, description, imageUrl, tags, detailedDescription, 
     )
 }
 
+
 export default function ProjectCatalog() {
     const projects = [
         {
@@ -78,7 +83,7 @@ export default function ProjectCatalog() {
             description: "Your Ai Partner for transcribing and understanding human speech..",
             imageUrl: wizardAi,
             tags: ["AI", "LLM", "AI Saas", "Devops"],
-            detailedDescription: "Designed and Introduced a scalable web application for video summarization using Flask framework, which would transcribe and summarize the video.",
+            detailedDescription: "Designed and Introduced a scalable web application for video summarization using Flask framework, which would transcribe and summarize the video.\n\nSpearheaded the fine-tuning of Facebook BART model for summarization, resulting in a 40% improvement in summarization quality and reducing the time spent on manual summarization by 50%.",
             techStack: ["Python 3.8", "Large Language Models", "Facebook BARt", "CI/CD", "Open AI", "Whisper", "Git", "Flask", "Tailwind CSS", "JavaScript"],
             websiteUrl: "https://github.com/shashank1623/wizard_ai"
         },
@@ -87,7 +92,7 @@ export default function ProjectCatalog() {
             description: "Create, edit, and share documents seamlessly. Experience the future of collaborative document editing.",
             imageUrl: Docsync,
             tags: ["React", "Nodejs", "PostgreSQL"],
-            detailedDescription: "Developed a real-time collaborative document editing platform supporting simultaneous multi-user editing and real-time updates, achieving a 30% reduction in document editing latency.",
+            detailedDescription: "Developed a real-time collaborative document editing platform supporting simultaneous multi-user editing and real-time updates, achieving a 30% reduction in document editing latency.\n\nImplemented secure JWT-based authentication and utilized Node.js, Express.js, Prisma ORM, and PostgreSQL for backend services, handling up to 200 concurrent users efficiently.\n\n",
             techStack: ["React", "Node.js", "Express", "MongoDB", "Zod", "Zustand", "WebSockets", "PostgreSQL"],
             websiteUrl: "https://docsync.shashankbhardwaj.me/"
         },
@@ -96,7 +101,7 @@ export default function ProjectCatalog() {
             description: "An application for tracking your flight details and status in real-time.",
             imageUrl: flightStatus,
             tags: ["React", "Flask", "Real Time"],
-            detailedDescription: "Developed a web application using a microservices architecture that enables users to search for flights, view upcoming arrivals and departures, and receive real-time notifications on flight status.",
+            detailedDescription: "Developed a web application using a microservices architecture that enables users to search for flights, view upcoming arrivals and departures, and receive real-time notifications on flight status.\n\nIntegrated a notification system using Redis for real-time updates on gate numbers and baggage carousel information, improving user experience by 30%.",
             techStack: ["React", "Flask", "MongoDB", "AWS", "Redis", "Git"],
             websiteUrl: "https://flight-status-system-u755.vercel.app/"
         },
@@ -106,7 +111,7 @@ export default function ProjectCatalog() {
             imageUrl: RetailReadyAI,
             tags: ["Nextjs", "React", "CI/CD"],
             detailedDescription: "Designed and developed a modern, responsive UI for RetailReady, featuring a clean layout with a focus on simplicity and user engagement.\n\nThe project involved:Using Next.js with TypeScript for building a dynamic and scalable front-end.Implementing Tailwind CSS for seamless responsive design and custom styling.Ensuring cross-browser compatibility and optimal performance on both desktop and mobile devices.Leveraging reusable components and maintaining design consistency across the platform.\n\nThis project highlights my ability to create visually appealing and user-friendly UI components that enhance user engagement and contribute to a cohesive brand experience.",
-            techStack: ["Nextjs", "React", "Tailwind CSS", "AWS", "CI/CD", "Git" , "Framer-motion"],
+            techStack: ["Nextjs", "React", "Tailwind CSS", "AWS", "CI/CD", "Git", "Framer-motion"],
             websiteUrl: "https://retailreadyai.shashankbhardwaj.me/"
         }
     ]
